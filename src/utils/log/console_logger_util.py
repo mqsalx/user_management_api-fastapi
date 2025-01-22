9e3f6fcde9f6cc1c7785f9f12cbc9bdebd5acac8
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-01-21 21:35:22 -0300

    feat: add ConsoleLoggerUtility class for colored console logging
    - Add support for colored log messages (info, warning, error)
    - Include color codes for RED, GREEN, YELLOW, and RESET
    - Provide methods for logging info, warnings, and errors"

---
+++ src/utils/log/console_logger_util.py
@@ -0,0 +1,33 @@
+# /src/utils/log/console_logger_util.py
+
+
+class ConsoleLoggerUtil:
+    """
+
+    Console logger utility.
+
+    """
+
+    def __init__(self):
+
+        self.__color = {
+            "RED": "\033[91m",
+            "GREEN": "\033[92m",
+            "YELLOW": "\033[93m",
+            "RESET": "\033[0m",
+        }
+
+    def log_info(self, message: str):
+        color = self.__color["GREEN"]
+        reset = self.__color["RESET"]
+        print(f"{color}[INFO]{reset} {message}")
+
+    def log_warning(self, message: str):
+        color = self.__color["YELLOW"]
+        reset = self.__color["RESET"]
+        print(f"{color}[WARNING]{reset} {message}")
+
+    def log_error(self, message: str):
+        color = self.__color["RED"]
+        reset = self.__color["RESET"]
+        print(f"{color}[ERROR]{reset} {message}")
