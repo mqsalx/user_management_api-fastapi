f2144d90ebb5e838945fdea1453f0648b0508974
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-01-21 21:34:17 -0300

    feat: add EnvConfiguration class for environment settings
    - Add support for API host, port, and prefix configuration
    - Add database URL configuration
    - Add JWT secret key, algorithm, and token expiration settings"

---
+++ src/core/configurations/env_configuration.py
@@ -0,0 +1,58 @@
+# /src/core/env/env_variable.py
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class EnvConfiguration:
+
+    def __init__(self):
+
+        # API Setup
+        self.__api_host = str(os.getenv("API_HOST"))
+        self.__api_port = int(os.getenv("API_PORT"))  # type: ignore
+        self.__api_prefix = str(os.getenv("API_PREFIX"))
+
+        # Database Setup
+        self.__db_url = str(os.getenv("DATABASE_URL"))
+
+        # JWT Setup
+        self.__jwt_secret_key = str(os.getenv("SECRET_KEY"))
+        self.__jwt_algorithm = str(os.getenv("JWT_ALGORITHM"))
+        self.__jwt_access_token_expire_minutes = int(
+            os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES")  # type: ignore
+        )
+
+    # API Setup
+    @property
+    def api_host(self) -> str:
+        return self.__api_host
+
+    @property
+    def api_port(self) -> int:
+        return self.__api_port
+
+    @property
+    def api_prefix(self) -> str:
+        return self.__api_prefix
+
+    # Database Setup
+    @property
+    def db_url(self) -> str:
+        return self.__db_url
+
+    # JWT Setup
+    @property
+    def jwt_secret_key(self) -> str:
+        return self.__jwt_secret_key
+
+    @property
+    def jwt_algorithm(self) -> str:
+        return self.__jwt_algorithm
+
+    @property
+    def jwt_access_token_expire_minutes(self) -> int:
+        return self.__jwt_access_token_expire_minutes
