1cc0b4d1bc3ad873aae46ec26f0e392b922ccd6d
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-02-01 10:33:07 -0300

    refactor: improved structure and removal of unused code sections

--- src/usecases/user/user_usecase.py
+++ src/usecases/user/user_usecase.py
@@ -1,12 +1,12 @@
 # /src/usecases/user/user_usecase.py
 
+from src.core.dtos.user.user_dto import UserRequestDTO, UserResponseDTO
 from src.core.entities.user.user_entity import UserEntity
 from src.core.exceptions.base.base_exception import BaseException
 from src.core.exceptions.usecases.user.user_exception import (
     EmailAlreadyExistsException,
     UserNotFoundException,
 )
-from src.core.dtos.user.user_dto import UserRequestDTO, UserResponseDTO
 from src.infrastructure.repositories.user.user_repository import UserRepository
 from src.utils.log.console_logger_util import ConsoleLoggerUtil
 
@@ -24,13 +24,16 @@ class UserUseCase:
     def create_user(self, request: UserRequestDTO) -> UserResponseDTO:
 
         try:
-            if self.__repository.get_email(request.email):
-                raise EmailAlreadyExistsException(request.email)
+            name = request.name
+            email = request.email
+            status = request.status
+            if self.__repository.get_email(email):
+                raise EmailAlreadyExistsException(email)
 
             entity = UserEntity(
-                name=request.name,
-                email=request.email,
-                status=request.status,
+                name=name,
+                email=email,
+                status=status,
             )
 
             return self.__repository.create_user(entity)
@@ -38,10 +41,11 @@ class UserUseCase:
             CONSOLE_LOGGER_ERROR(f"Error creating user: {error}")
             raise
 
-
     def get_users(self):
         users = self.__repository.get_users()
-        return {UserResponseDTO.model_validate(users.__dict__) for user in users}
+        return {
+            UserResponseDTO.model_validate(users.__dict__) for user in users
+        }
 
     def get_user(self, user_id: int) -> UserResponseDTO:
         user = self.__repository.get_user(user_id)
