7f5443793c7cbce4dcb90d6c71567e6bb7ba2177
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-02-01 10:30:14 -0300

    refactor: modified inheritance

--- src/core/exceptions/base/base_exception_handler.py
+++ src/core/exceptions/base/base_exception_handler.py
@@ -1,20 +1,20 @@
 # src/core/exceptions/base/base_exception_handler.py
 
-from fastapi import Request
-from fastapi.responses import JSONResponse
+from http import HTTPStatus
 
-from src.core.exceptions.base.base_exception import BaseException
+from fastapi import HTTPException, Request
+from fastapi.responses import JSONResponse
 
 
 class BaseExceptionHandler:
 
     @staticmethod
-    async def handler(request: Request, exc: BaseException) -> JSONResponse:
+    async def handler(request: Request, exc: HTTPException) -> JSONResponse:
         return JSONResponse(
             status_code=exc.status_code,
             content={
                 "status_code": str(exc.status_code),
-                "status_name":
+                "status_name": HTTPStatus(exc.status_code).phrase,
                 "message": exc.detail,
             },
         )
