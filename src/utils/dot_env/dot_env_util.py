dc19e55084549465ac8eb9b935bf11f8891a729f
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-02-06 21:08:52 -0300

    feat:
    - added functionality to validate .env loading

---
+++ src/utils/dot_env/dot_env_util.py
@@ -0,0 +1,24 @@
+# /src/utils/dot_env/dot_env_util.py
+
+
+from dotenv import find_dotenv
+
+
+class DotEnvUtil:
+
+    def __init__(self):
+
+        self.__env_path = find_dotenv()
+
+    def check_dot_env(self) -> None:
+
+        additional_message = "ENV VARIABLES -> "
+
+        if not self.__env_path:
+            message = (
+                f"\n\033[32m\033[1m{additional_message}The file .env is not loaded! ... running default env variables\033[0m\n"
+            )
+            print(message)
+
+        message = f"\n\033[32m\033[1m{additional_message}The file .env is loaded!\033[0m\n"
+        print(message)
