690ae4a63e848022947c79457cbeec79e993e9fd
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-03-19 19:23:20 -0300

    feat:
    - enhance validation in DTOs by adding enum validation
    - update mode handling

--- src/core/dtos/base_dto.py
+++ src/core/dtos/base_dto.py
@@ -1,57 +1,58 @@
 from enum import Enum
-from typing import Any
+from typing import Any, Type
 
-from pydantic import (
-    BaseModel,
-    ValidationInfo,
-    field_validator,
-    model_validator,
-)
+from pydantic import BaseModel, model_validator
 
 from src.core.exceptions.dto_exception import (
     InvalidExtraFieldsException,
     MissingRequiredFieldsException,
     OnlyAcceptsValuesException,
 )
+from src.utils.logger_util import LoggerUtil
+
+log = LoggerUtil()
 
 
 class BaseDTO(BaseModel):
 
-    @field_validator("*", mode="before")
+    __update_mode__: bool = False
+
     @classmethod
-    def validate_enum(cls: Any, value: Any, info: ValidationInfo) -> Any:
+    def validate_enum(cls, value: str, info, enum_type: Type[Enum]):
+        field_name = info.field_name
+        allowed_values = {role.value for role in enum_type}
 
-        field = info.field_name
-        field_type = cls.model_fields[field].annotation
+        if value in ["", " "]:
+            raise OnlyAcceptsValuesException(
+                f"The field {field_name} was given empty or null value! Allowed values: {', '.join(allowed_values)}"
+            )
 
-        if isinstance(field_type, type) and issubclass(field_type, Enum):
-            allowed_values = [e.value for e in field_type]
-            if value not in allowed_values:
-                if value is None or value == "":
-                    value = "empty or null"
-                raise OnlyAcceptsValuesException(
-                    f"The {field} field received {value}! Allowed values: {", ".join(allowed_values)}"
-                )
+        if value not in allowed_values:
+            raise OnlyAcceptsValuesException(
+                f"The field {field_name} was given {value}, which is invalid! Allowed values: {', '.join(allowed_values)}"
+            )
 
         return value
 
     @model_validator(mode="before")
     @classmethod
     def validate_fields(cls, values: Any) -> Any:
+
         allowed_fields = set(cls.model_fields.keys())
         received_fields = set(values.keys())
 
         extra_fields = received_fields - allowed_fields
-        missing_fields = allowed_fields - received_fields
 
         if extra_fields:
             raise InvalidExtraFieldsException(
                 f'Additional fields not allowed: {", ".join(extra_fields)}'
             )
 
-        if missing_fields:
-            raise MissingRequiredFieldsException(
-                f'Required fields missing: {", ".join(missing_fields)}'
-            )
+        if not cls.__update_mode__:
+            missing_fields = allowed_fields - received_fields
+            if missing_fields:
+                raise MissingRequiredFieldsException(
+                    f'Required fields missing: {", ".join(missing_fields)}'
+                )
 
         return values
--- src/core/dtos/user_dto.py
+++ src/core/dtos/user_dto.py
@@ -3,7 +3,7 @@
 
 from typing import Dict, List, Union
 
-from pydantic import BaseModel, EmailStr, RootModel
+from pydantic import EmailStr, RootModel, field_validator
 
 from src.core.dtos.base_dto import BaseDTO
 from src.core.enums.user_status_enum import UserStatusEnum
@@ -15,12 +15,25 @@ class CreateUserRequestDTO(BaseDTO):
     status: UserStatusEnum = UserStatusEnum.ACTIVE
     password: str
 
+    @field_validator("status", mode="before")
+    @classmethod
+    def validate_status(cls, value, info):
+        return cls.validate_enum(value, info, UserStatusEnum)
 
-class UpdateUserRequestDTO(BaseModel):
+
+class UpdateUserRequestDTO(BaseDTO):
+
+    __update_mode__ = True
+
     name: str | None = None
     email: EmailStr | None = None
     status: UserStatusEnum | None = None
     password: str | None = None
+
+    @field_validator("status", mode="before")
+    @classmethod
+    def validate_status(cls, value, info):
+        return cls.validate_enum(value, info, UserStatusEnum)
 
 
 class UserResponseDTO(RootModel):
