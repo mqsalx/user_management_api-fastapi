fa970a53839640dd1cddbfd1653007c0a21ddfb4
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-06-01 10:28:37 -0300

    refactor(repositories): file/folder structure organization

---
+++ src/data/repositories/__init__.py
@@ -0,0 +1,6 @@
+# /src/data/repository/__init__.py
+
+# flake8: noqa: E501, F401
+
+from src.data.repositories.auth import AuthRepository
+from src.data.repositories.user import UserRepository
--- src/data/repository/__init__.py
+++
@@ -1,6 +0,0 @@
-# /src/data/repository/__init__.py
-
-# flake8: noqa: E501, F401
-
-from src.data.repository.auth import AuthRepository
-from src.data.repository.user import UserRepository
