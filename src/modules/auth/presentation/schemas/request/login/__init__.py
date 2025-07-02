bbd6128e3c3c6caa5f57744b7ca27b851c68ba78
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-07-02 18:58:46 -0300

    feat(auth): add LoginRequest DTO for user login validation

---
+++ src/modules/auth/presentation/schemas/request/login/__init__.py
@@ -0,0 +1,19 @@
+# /src/domain/dtos/request/body/auth/login/__init__.py
+
+# flake8: noqa: E501
+
+from src.domain.dtos.base import BaseDTO
+
+
+class LoginRequest(BaseDTO):
+    """
+    Class responsible for the Data Transfer Object (DTO) for user login requests.
+
+    This class is used to validate and structure login request payloads.
+
+    Class Args:
+        None
+    """
+
+    email: str
+    password: str
