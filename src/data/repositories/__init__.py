3a79e717cc5ba6f1d435b0b1f91cd425bd30c433
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-07-05 20:39:52 -0300

    refactor(repositories): remove unused authentication and user repository files

--- src/data/repositories/__init__.py
+++
@@ -1,8 +0,0 @@
-# /src/data/repository/__init__.py
-
-# flake8: noqa: E501, F401
-
-from src.data.repositories.auth.login import LoginRepository
-from src.data.repositories.auth.token import TokenRepository
-from src.data.repositories.auth.session import SessionAuthRepository
-from src.data.repositories.user import UserRepository
