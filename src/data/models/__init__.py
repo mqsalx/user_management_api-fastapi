a44ea78ea39314bdaa899d774efeda1faa69f165
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-07-05 20:39:24 -0300

    refactor(models): remove unused model files for authentication, permissions, roles, and users

--- src/data/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# /src/data/models/__init__.py
-
-# flake8: noqa: E501, F401
-
-from src.data.models.auth.session import SessionAuthModel
-from src.data.models.auth.token import TokenModel
-from src.data.models.permission import PermissionModel
-from src.data.models.role import RoleModel
-# from src.data.models.user import UserModel
